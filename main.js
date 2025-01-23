49244b8810fd00287146ab26de2c1d5559209c33
PancakePrograms <charliemaerose8@gmail.com>
2025-01-23 08:29:23 -0500

    update

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const secret = document.querySelector(".secret");
 const links = document.querySelector(".links")
 
+const body = document.querySelector("body")
+
 document.addEventListener("dblclick", () => {
   
   if (secret.style.display === "none" || secret.style.display === "") {
-    secret.style.display = "block";
+    body.style.backgroundImage = "url(/Untitled_Artwork.jpg)";
+    secret.style.display = "block"
     links.style.display = "none";
   } else {
     secret.style.display = "none";
