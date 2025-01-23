ac4228e682e6a8c98330f1b5b008e5b78dd5032a
PancakePrograms <charliemaerose8@gmail.com>
2025-01-23 02:47:21 -0500

    update

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+const secret = document.querySelector(".secret");
+const links = document.querySelector(".links")
+
+document.addEventListener("dblclick", () => {
+  
+  if (secret.style.display === "none" || secret.style.display === "") {
+    secret.style.display = "block";
+    links.style.display = "none";
+  } else {
+    secret.style.display = "none";
+    links.style.display = "flex"
+  }
+})
